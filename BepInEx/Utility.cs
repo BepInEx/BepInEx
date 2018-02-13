849e722c1ad8dd2a70b19c457793aac29fc19da3
Bepis <36346617+bbepis@users.noreply.github.com>
2018-02-13 18:22:32 +1100

    Basic plugin loader

--- BepInEx/Utility.cs
+++ BepInEx/Utility.cs
@@ -10,6 +10,7 @@ namespace BepInEx
     public static class Utility
     {
         public static string ExecutingDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace(@"file:\", "");
+        public static string PluginsDirectory => Path.Combine(ExecutingDirectory, "BepInEx");
 
         //shamelessly stolen from Rei
         public static string CombinePaths(params string[] parts) => parts.Aggregate(Path.Combine);
