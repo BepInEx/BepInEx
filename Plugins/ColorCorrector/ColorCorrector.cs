a46c06047e8d3ff9d866edcba3e18472b877836f
Bepis <36346617+bbepis@users.noreply.github.com>
2018-02-14 18:04:32 +1100

    Added a color correction plugin

---
+++ Plugins/ColorCorrector/ColorCorrector.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ColorCorrector
+{
+    public class ColorCorrector : BaseUnityPlugin
+    {
+        public override string Name => "Color Corrector";
+
+        protected override void LevelFinishedLoading(Scene scene, LoadSceneMode mode)
+        {
+            var c = Camera.main.gameObject.GetComponent("AmplifyColorEffect");
+            Destroy(c);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Koikatsu patcher
 
 ## Credits
-- Usagirei - Code for using the console and for being very helpful
+- Usagirei - Color Correction plugin, code for using the console and for being very helpful
 - Illusion Plugin Architecture (IPA) - Code for GameObject loading/framework
