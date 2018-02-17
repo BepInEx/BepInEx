90e6812f2d5a41412d3d5f74a438ac58b3bb0242
Bepis <36346617+bbepis@users.noreply.github.com>
2018-02-18 10:32:04 +1100

    Initial resource redirector class

---
+++ Plugins/ResourceRedirector/ResourceRedirector.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.SceneManagement;
+
+namespace ResourceRedirector
+{
+    public class ResourceRedirector : BaseUnityPlugin
+    {
+        public override string Name => "Resource Redirector";
+
+        protected override void LevelFinishedLoading(Scene scene, LoadSceneMode mode)
+        {
+            
+        }
+
+
+    }
+}
