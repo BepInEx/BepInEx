bf235c9659e2de9e32e710e8bd3be843553f6a9c
Bepis <36346617+bbepis@users.noreply.github.com>
2018-02-23 15:00:33 +1100

    Added a plugin to append extra data to cards

---
+++ Plugins/ExtensibleSaveFormat/ExtensibleSaveFormat.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensibleSaveFormat
+{
+    public class ExtensibleSaveFormat : BaseUnityPlugin
+    {
+        public override string ID => "extendedsave";
+
+        public override string Name => "Extensible Save Format";
+
+        public override Version Version => new Version("1.0");
+
+
+        internal static Dictionary<ChaFile, Dictionary<string, object>> internalDictionary = new Dictionary<ChaFile, Dictionary<string, object>>();
+
+        public ExtensibleSaveFormat()
+        {
+            Hooks.InstallHooks();
+        }
+
+
+        public static bool TryGetExtendedFormat(ChaFile file, out Dictionary<string, object> extendedFormatData)
+        {
+            return internalDictionary.TryGetValue(file, out extendedFormatData);
+        }
+
+        public static void SetExtendedFormat(ChaFile file, Dictionary<string, object> extendedFormatData)
+        {
+            internalDictionary[file] = extendedFormatData;
+        }
+    }
+}
---
+++ Plugins/ExtensibleSaveFormat/Hooks.cs
@@ -0,0 +1,71 @@
+﻿using Harmony;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ExtensibleSaveFormat
+{
+    public static class Hooks
+    {
+        public static void InstallHooks()
+        {
+            var harmony = HarmonyInstance.Create("com.bepis.bepinex.extensiblesaveformat");
+
+
+            MethodInfo original = AccessTools.Method(typeof(ChaFile), "SaveFile", new[] { typeof(BinaryWriter), typeof(bool) });
+
+            HarmonyMethod postfix = new HarmonyMethod(typeof(Hooks).GetMethod("SaveFileHook"));
+
+            harmony.Patch(original, null, postfix);
+
+
+            original = AccessTools.Method(typeof(ChaFile), "LoadFile", new[] { typeof(BinaryReader), typeof(bool), typeof(bool) });
+
+            postfix = new HarmonyMethod(typeof(Hooks).GetMethod("LoadFileHook"));
+
+            harmony.Patch(original, null, postfix);
+        }
+
+        public static void SaveFileHook(ChaFile __instance, bool __result, BinaryWriter bw, bool savePng)
+        {
+            if (!__result)
+                return;
+
+            if (!ExtensibleSaveFormat.TryGetExtendedFormat(__instance, out Dictionary<string, object> extendedData))
+                return;
+
+            byte[] bytes = MessagePackSerializer.Serialize(extendedData);
+
+            bw.Write((int)bytes.Length);
+            bw.Write(bytes);
+        }
+
+        public static void LoadFileHook(ChaFile __instance, bool __result, BinaryReader br, bool noLoadPNG, bool noLoadStatus)
+        {
+            if (!__result)
+                return;
+
+            try
+            {
+                int length = br.ReadInt32();
+
+                if (length > 0)
+                {
+                    byte[] bytes = br.ReadBytes(length);
+
+                    ExtensibleSaveFormat.internalDictionary[__instance] = MessagePackSerializer.Deserialize<Dictionary<string, object>>(bytes);
+
+                    return;
+                }
+            }
+            catch (EndOfStreamException) { }
+
+            //initialize a new dictionary since it doesn't exist
+            ExtensibleSaveFormat.internalDictionary[__instance] = new Dictionary<string, object>();
+        }
+    }
+}
