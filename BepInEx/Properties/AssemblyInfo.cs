66744ed5c06541353b0d9933f6e3dfb90f1b2878
denikson <geoffrey.hoooooorse@gmail.com>
2018-07-23 16:37:12 +0300

    Prevent future ReSharper cleanup in AssemblyInfo

--- BepInEx/Properties/AssemblyInfo.cs
+++ BepInEx/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+// ReSharper disable once RedundantUsingDirective
 using BepInEx;
 
 // General Information about an assembly is controlled through the following
