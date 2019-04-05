473c0e3b39355427640d85e21dfa53fa2d8d9b12
Bepis <36346617+bbepis@users.noreply.github.com>
2019-04-05 22:42:09 +1100

    Add crypto entrypoint

---
+++ BepInEx.Bootstrap/RngEntrypoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace BepInEx.Bootstrap
+{
+	[ComVisible(true)]
+	public class RngEntrypoint : RandomNumberGenerator
+	{
+		private RNGCryptoServiceProvider InternalProvider { get; }
+
+		static RngEntrypoint()
+		{
+			Entrypoint.Init();
+		}
+
+		public RngEntrypoint()
+			=> InternalProvider = new RNGCryptoServiceProvider();
+
+		public RngEntrypoint(string str)
+			=> InternalProvider = new RNGCryptoServiceProvider(str);
+
+		public RngEntrypoint(byte[] rgb)
+			=> InternalProvider = new RNGCryptoServiceProvider(rgb);
+
+		public RngEntrypoint(CspParameters cspParams)
+			=> InternalProvider = new RNGCryptoServiceProvider(cspParams);
+
+		public override void GetBytes(byte[] data)
+			=> InternalProvider.GetBytes(data);
+
+		public override void GetNonZeroBytes(byte[] data)
+			=> InternalProvider.GetNonZeroBytes(data);
+	}
+}
