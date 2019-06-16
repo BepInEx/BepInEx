363cdd5de145d3562b1522bc8edff86a291d8b22
ghorsington <neigh@coder.horse>
2019-06-16 16:43:28 +0300

    Read Managed folder path from Doorstop

--- BepInEx/Bootstrap/Chainloader.cs
+++ BepInEx/Bootstrap/Chainloader.cs
@@ -39,13 +39,15 @@ namespace BepInEx.Bootstrap
 		/// <summary>
         /// Initializes BepInEx to be able to start the chainloader.
         /// </summary>
-        public static void Initialize(string containerExePath, bool startConsole = true)
+        public static void Initialize(string containerExePath, string managedPath = null, bool startConsole = true)
 		{
 			if (_initialized)
 				return;
 
 			//Set vitals
-			Paths.SetExecutablePath(containerExePath, pluginPath: ConfigPluginsDirectory.Value);
+			Paths.SetExecutablePath(containerExePath);
+			Paths.SetManagedPath(managedPath);
+			Paths.SetPluginPath(ConfigPluginsDirectory.Value);
 
             //Start logging
             if (ConsoleWindow.ConfigConsoleEnabled.Value && startConsole)
