1795234511a2852cf842de72bd2b8ed2f42ababa
ghorsington <neigh@coder.horse>
2020-07-05 20:03:30 +0300

    Bump version

--- BepInEx/Properties/AssemblyInfo.cs
+++ BepInEx/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using BepInEx;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.0.0")]
-[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.2.0.0")]
+[assembly: AssemblyFileVersion("5.2.0.0")]
