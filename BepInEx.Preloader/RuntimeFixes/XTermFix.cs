25704c85610719e016858ad92bec35b846587248
Bepis <36346617+bbepis@users.noreply.github.com>
2020-08-27 02:05:46 +1000

    Revert back to System.ConsoleDriver check for XTermFix This is what we determined when we discovered that Unity 4.7 has a hybrid xterm implementation:
    * ConsoleDriver is not present => don't patch + use our tty driver
    * ConsoleDriver is present and ParameterizedStrings is not => patch + use our tty driver
    * ConsoleDriver is present and ParameterizedStrings is too => patch + use mono's tty driver

--- BepInEx.Preloader/RuntimeFixes/XTermFix.cs
+++ BepInEx.Preloader/RuntimeFixes/XTermFix.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
-using BepInEx.Unix;
 using HarmonyLib;
 using MonoMod.RuntimeDetour;
 using MonoMod.RuntimeDetour.Platforms;
@@ -17,7 +16,7 @@ namespace BepInEx.Preloader.RuntimeFixes
 			if (Utility.CurrentOs == Platform.Windows)
 				return;
 
-			if (!LinuxConsoleDriver.UseMonoTtyDriver)
+			if (typeof(Console).Assembly.GetType("System.ConsoleDriver") == null)
 			{
 				// Mono version is too old, use our own TTY implementation instead
 				return;
