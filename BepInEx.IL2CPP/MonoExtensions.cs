59237ec73999f5a65f769fe5607bf1fdc98af850
Bepis <36346617+bbepis@users.noreply.github.com>
2020-09-04 23:05:16 +1000

    Implement initial MonoExtensions class

---
+++ BepInEx.IL2CPP/MonoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace BepInEx
+{
+	/// <summary>
+	/// Contains unofficial extensions to the underlying Mono runtime.
+	/// </summary>
+	public static class MonoExtensions
+	{
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		private static extern IntPtr GetFunctionPointerForDelegateInternal2(Delegate d, CallingConvention conv);
+
+		public static IntPtr GetFunctionPointerForDelegate(Delegate d, CallingConvention conv)
+		{
+			if (d == null)
+			{
+				throw new ArgumentNullException("d");
+			}
+			return GetFunctionPointerForDelegateInternal2(d, conv);
+		}
+	}
+}
