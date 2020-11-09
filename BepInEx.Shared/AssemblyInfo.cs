094b8f5ba88c14374f5d6392465619f8c58d850f
ghorsington <neigh@coder.horse>
2020-11-09 19:14:17 +0200

    Add shared project with shared version info

---
+++ BepInEx.Shared/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly:AssemblyCopyright("Copyright © 2020 BepInEx Team")]
+[assembly: AssemblyVersion(VersionInfo.VERSION)]
+[assembly: AssemblyFileVersion(VersionInfo.VERSION)]
+[assembly: AssemblyInformationalVersion(VersionInfo.VERSION)]
+
+internal static class VersionInfo
+{
+	public const string VERSION = "6.0.0.0";
+}
