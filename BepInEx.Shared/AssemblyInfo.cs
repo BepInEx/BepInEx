8e3902317d04f91d1762cc457c2aa01cb88bdd4e
ghorsington <neigh@coder.horse>
2020-11-09 19:25:51 +0200

    Format AssemblyInfo

--- BepInEx.Shared/AssemblyInfo.cs
+++ BepInEx.Shared/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 // ReSharper disable once RedundantUsingDirective
 using BepInEx.Shared;
 
-[assembly:AssemblyCopyright("Copyright © 2020 BepInEx Team")]
+[assembly: AssemblyCopyright("Copyright © 2020 BepInEx Team")]
 [assembly: AssemblyVersion(VersionInfo.VERSION)]
 [assembly: AssemblyFileVersion(VersionInfo.VERSION)]
 [assembly: AssemblyInformationalVersion(VersionInfo.VERSION)]
