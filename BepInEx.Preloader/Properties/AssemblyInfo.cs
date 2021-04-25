d5d9c5c6bf9c8ea6c5f659f203b3b62106531a3b
ghorsington <neigh@coder.horse>
2021-04-25 22:06:53 +0300

    Bump version

--- BepInEx.Preloader/Properties/AssemblyInfo.cs
+++ BepInEx.Preloader/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.4.9.0")]
-[assembly: AssemblyFileVersion("5.4.9.0")]
+[assembly: AssemblyVersion("5.4.10.0")]
+[assembly: AssemblyFileVersion("5.4.10.0")]
