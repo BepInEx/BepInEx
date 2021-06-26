3a2c4fc526517702398d847ff3cf66128665320d
ghorsington <neigh@coder.horse>
2021-06-26 22:20:09 +0300

    Add initial configuration provider API

---
+++ BepInEx.Core/Configuration/ConfigurationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BepInEx.Configuration
+{
+    public abstract class ConfigurationProvider
+    {
+        public IDictionary<string, string> RawData { get; }
+
+        public abstract void Load();
+
+        public abstract void Save();
+    }
+}
