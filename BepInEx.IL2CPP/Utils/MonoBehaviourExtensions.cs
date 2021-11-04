4530c98ab2e226882dcf322a32ddfd5718a97811
ghorsington <neigh@coder.horse>
2021-11-04 22:48:38 +0200

    Il2Cpp: Add MonoBehaviour.StartCoroutine extension with managed IEnumerator

---
+++ BepInEx.IL2CPP/Utils/MonoBehaviourExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using BepInEx.IL2CPP.Utils.Collections;
+using UnityEngine;
+
+namespace BepInEx.IL2CPP.Utils
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static Coroutine StartCoroutine(this MonoBehaviour self, IEnumerator coroutine)
+        {
+            return self.StartCoroutine(coroutine.WrapToIl2Cpp());
+        }
+    }
+}
