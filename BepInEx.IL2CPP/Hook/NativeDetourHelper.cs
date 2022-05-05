4db21f007b315314d84605c9cb3af03399471195
Kasuromi <29818914+Kasuromi@users.noreply.github.com>
2022-05-05 22:47:26 +0300

    Add Dobby Detour Provider

--- BepInEx.IL2CPP/Hook/NativeDetourHelper.cs
+++ BepInEx.IL2CPP/Hook/NativeDetourHelper.cs
@@ -1,3 +1,4 @@
+using BepInEx.IL2CPP.Hook.Dobby;
 using BepInEx.IL2CPP.Hook.Funchook;
 using System;
 using System.Runtime.InteropServices;
@@ -9,7 +10,8 @@ public class NativeDetourHelper
     public static bool DEBUG_DETOURS = false;
     public static INativeDetour Create(nint original, nint target)
     {
-        return new FunchookDetour(original, target);
+        return new DobbyDetour(original, target);
+        //return new FunchookDetour(original, target);
     }
 
     public static INativeDetour CreateAndApply<T>(nint from, T to, out T original, CallingConvention? callingConvention = null)
