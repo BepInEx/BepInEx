8dd29b764b6263a79fe2dc4d38d07e46e2a3c8dc
ghorsington <neigh@coder.horse>
2022-07-10 20:46:46 +0300

    Use new version handler for Il2CppInteropManager

--- BepInEx.IL2CPP/Preloader.cs
+++ BepInEx.IL2CPP/Preloader.cs
@@ -1,36 +1,23 @@
 using System;
-using System.Diagnostics;
-using System.Globalization;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using BepInEx.Configuration;
 using BepInEx.Logging;
 using BepInEx.Preloader.Core;
 using BepInEx.Preloader.Core.Logging;
 using BepInEx.Preloader.Core.Patching;
 using BepInEx.Preloader.RuntimeFixes;
+using BepInEx.Unity.Core;
 
 namespace BepInEx.IL2CPP;
 
 public static class Preloader
 {
-    #region Config
-
-    private static readonly ConfigEntry<string> ConfigUnityVersion = ConfigFile.CoreConfig.Bind(
-     "IL2CPP", "UnityVersion",
-     string.Empty,
-     "Unity version to report to Il2CppUnhollower. If empty, version is automatically determined from the game process.");
-
-    #endregion
-
     private static PreloaderConsoleListener PreloaderLog { get; set; }
 
     internal static ManualLogSource Log => PreloaderLogger.Log;
 
     // TODO: This is not needed, maybe remove? (Instance is saved in IL2CPPChainloader itself)
     private static IL2CPPChainloader Chainloader { get; set; }
-
-    public static Version UnityVersion { get; private set; }
 
     public static void Run()
     {
@@ -62,8 +49,7 @@ public static class Preloader
 
             NativeLibrary.SetDllImportResolver(typeof(Il2CppInterop.Runtime.IL2CPP).Assembly, DllImportResolver);
 
-            InitializeUnityVersion();
-            Il2CppInteropManager.Initialize(UnityVersion);
+            Il2CppInteropManager.Initialize();
 
             using (var assemblyPatcher = new AssemblyPatcher((data, _) => Assembly.Load(data)))
             {
@@ -102,51 +88,5 @@ public static class Preloader
         }
 
         return IntPtr.Zero;
-    }
-
-    private static void InitializeUnityVersion()
-    {
-        if (TryInitializeUnityVersion(ConfigUnityVersion.Value))
-            Log.Log(LogLevel.Warning, "Unity version obtained from the config.");
-        else if (TryInitializeUnityVersion(Process.GetCurrentProcess().MainModule.FileVersionInfo.FileVersion))
-            Log.Log(LogLevel.Debug, "Unity version obtained from main application module.");
-        else
-            Log.Log(LogLevel.Error, "Running under default Unity version. UnityVersionHandler is not initialized.");
-    }
-
-    private static bool TryInitializeUnityVersion(string version)
-    {
-        try
-        {
-            if (string.IsNullOrWhiteSpace(version))
-                return false;
-
-            var parts = version.Split('.');
-            var major = 0;
-            var minor = 0;
-            var build = 0;
-
-            // Issue #229 - Don't use Version.Parse("2019.4.16.14703470L&ProductVersion")
-            var success = int.TryParse(parts[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out major);
-            if (success && parts.Length > 1)
-                success = int.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out minor);
-            if (success && parts.Length > 2)
-                success = int.TryParse(parts[2], NumberStyles.Integer, CultureInfo.InvariantCulture, out build);
-
-            if (!success)
-            {
-                Log.LogError($"Failed to parse Unity version: {version}");
-                return false;
-            }
-
-            UnityVersion = new Version(major, minor, build);
-            Log.LogInfo($"Running under Unity v{UnityVersion}");
-            return true;
-        }
-        catch (Exception ex)
-        {
-            Log.LogError($"Failed to parse Unity version: {ex}");
-            return false;
-        }
     }
 }
