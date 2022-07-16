008f835e809453ce3fd2eeb24052348752cc5d81
ghorsington <neigh@coder.horse>
2022-07-16 19:37:38 +0300

    Port Cakebuild script to Cake.Frosting

--- build/DownloadTasks.cs
+++ build/DownloadTasks.cs
@@ -4,57 +4,69 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Nuke.Common.IO;
-using ShellProgressBar;
+using Cake.Core;
+using Cake.Core.IO;
+using Spectre.Console;
+using Path = System.IO.Path;
 
 static class DownloadTasks
 {
-    public static void DownloadFiles(string name, params (string Name, string Url, AbsolutePath Destination)[] files)
+    public static void DownloadFiles(this ICakeContext ctx,
+                                     string name,
+                                     params (string Name, string Url, FilePath Destination)[] files)
     {
-        using var progress = new ProgressBar(files.Length, $"Downloading {name}");
-        Task.WaitAll(files.Select(t => DownloadFile(progress, t.Name, t.Url, t.Destination)).ToArray());
-    }
-
-    public static void DownloadZipFiles(string name, params (string Name, string Url, AbsolutePath Destination)[] files)
-    {
-        using var progress = new ProgressBar(files.Length, $"Downloading {name}");
-        Task.WaitAll(files.Select(async t =>
+        AnsiConsole.Progress().Start(pCtx =>
         {
-            var zipFilePath = (AbsolutePath) $"{t.Destination}_tmp.zip";
-            await DownloadFile(progress, t.Name, t.Url, zipFilePath);
-            await UnzipFile(progress, t.Name, zipFilePath, t.Destination);
-            File.Delete(zipFilePath!);
-        }).ToArray());
+            Task.WaitAll(files
+                         .Select(t => DownloadFile(pCtx, t.Name, t.Url, t.Destination))
+                         .ToArray());
+        });
     }
 
-    static async Task DownloadFile(ProgressBarBase bar, string name, string url, AbsolutePath destination)
+    public static void DownloadZipFiles(this ICakeContext ctx,
+                                        string name,
+                                        params (string Name, string Url, DirectoryPath Destination)[] files)
+    {
+        AnsiConsole.Progress().Start(pCtx =>
+        {
+            Task.WaitAll(files.Select(async t =>
+            {
+                var zipFilePath = $"{t.Destination}_tmp.zip";
+                await DownloadFile(pCtx, t.Name, t.Url, zipFilePath);
+                await UnzipFile(pCtx, t.Name, zipFilePath, t.Destination);
+                File.Delete(zipFilePath!);
+            }).ToArray());
+        });
+    }
+
+    static async Task DownloadFile(ProgressContext pCtx, string name, string url, FilePath destination)
     {
         using var client = new HttpClient();
-        await using var fs = File.Create(destination);
+        await using var fs = File.Create(destination.FullPath);
         var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
         if (!response.IsSuccessStatusCode)
             throw new Exception($"Failed to download {name}");
         await using var stream = await response.Content.ReadAsStreamAsync();
-        using var childBar = bar.Spawn((int) (response.Content.Headers.ContentLength ?? 1), $"Downloading {name}");
+        var bar = pCtx.AddTask($"Downloading {name}", maxValue: (int) (response.Content.Headers.ContentLength ?? 1));
 
         var buffer = new byte[4096];
         int read;
         while ((read = await stream.ReadAsync(buffer)) > 0)
         {
             await fs.WriteAsync(buffer.AsMemory(0, read));
-            childBar.Tick(childBar.CurrentTick + read);
+            bar.Increment(read);
         }
     }
 
-    static async Task UnzipFile(ProgressBarBase bar, string name, AbsolutePath zipFile, AbsolutePath destination)
+    static async Task UnzipFile(ProgressContext pCtx, string name, FilePath zipFile, DirectoryPath destination)
     {
-        using var zip = ZipFile.OpenRead(zipFile);
-        using var childBar = bar.Spawn((int) zip.Entries.Select(e => e.Length).Sum(), $"Extracting {name}");
+        using var zip = ZipFile.OpenRead(zipFile.FullPath);
+        var bar = pCtx.AddTask($"Extracting {name}", maxValue: (int) zip.Entries.Select(e => e.Length).Sum());
 
         foreach (var entry in zip.Entries)
         {
             // Use Path.Combine to ensure the trailing slash is preserved.
-            var fullPath = Path.Combine(destination, entry.FullName);
+            var fullPath = Path.Combine(destination.FullPath, entry.FullName);
             var fileName = Path.GetFileName(fullPath);
             if (string.IsNullOrEmpty(fileName))
             {
@@ -70,7 +82,7 @@ static class DownloadTasks
             while ((read = await stream.ReadAsync(buffer)) > 0)
             {
                 await fs.WriteAsync(buffer.AsMemory(0, read));
-                childBar.Tick(childBar.CurrentTick + read);
+                bar.Increment(read);
             }
         }
     }
