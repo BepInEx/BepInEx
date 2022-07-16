1173da2bc667dcbe07713b15b6312a7a85815db0
ghorsington <neigh@coder.horse>
2022-07-16 19:35:35 +0300

    Restructure project names and folder layout

---
+++ Runtimes/NET/BepInEx.NET.Common/NetChainloader.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Reflection;
+using BepInEx.Bootstrap;
+using BepInEx.Preloader.Core.Logging;
+
+namespace BepInEx.NET.Common
+{
+    public class NetChainloader : BaseChainloader<BasePlugin>
+    {
+        // TODO: Remove once proper instance handling exists
+        public static NetChainloader Instance { get; set; }
+
+        public override void Initialize(string gameExePath = null)
+        {
+            Instance = this;
+            base.Initialize(gameExePath);
+        }
+
+        public override BasePlugin LoadPlugin(PluginInfo pluginInfo, Assembly pluginAssembly)
+        {
+            var type = pluginAssembly.GetType(pluginInfo.TypeName);
+
+            var pluginInstance = (BasePlugin) Activator.CreateInstance(type);
+
+            pluginInstance.Load();
+
+            return pluginInstance;
+        }
+
+        protected override void InitializeLoggers()
+        {
+            base.InitializeLoggers();
+
+            ChainloaderLogHelper.RewritePreloaderLogs();
+        }
+    }
+}
---
+++ build/DistributionTarget.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+readonly record struct DistributionTarget(string DistributionIdentifier, string RuntimeIndentifier)
+{
+    public readonly string Arch = RuntimeIndentifier.Split('-')[1];
+    public readonly string Engine = DistributionIdentifier.Split('.')[0];
+    public readonly string Os = RuntimeIndentifier.Split('-')[0];
+    public readonly string Runtime = DistributionIdentifier.Split('.')[1];
+    public readonly string Target = $"{DistributionIdentifier}-{RuntimeIndentifier}";
+
+    public string ClearOsName => Os switch
+    {
+        "win"   => "Windows",
+        "linux" => "Linux",
+        "macos" => "macOS",
+        var _   => throw new NotSupportedException($"OS {Os} is not supported")
+    };
+
+    public string DllExtension => Os switch
+    {
+        "win"   => "dll",
+        "linux" => "so",
+        "macos" => "dylib",
+        var _   => throw new NotSupportedException($"Unsupported OS: {Os}")
+    };
+
+    public string DllPrefix => Os switch
+    {
+        "win"   => "",
+        "linux" => "lib",
+        "macos" => "lib",
+        _       => throw new NotSupportedException($"Unsupported OS: {Os}")
+    };
+}
