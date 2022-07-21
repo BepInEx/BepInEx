3559d95a6ab8e0822083c07b31507879ccff6b41
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-07-21 17:40:50 +0200

    add ts v1 manifest json object structure

---
+++ BepInEx/Contract/ThunderstoreV1Manifest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BepInEx
+{
+    internal class ThunderstoreManifestV1
+    {
+        [JsonProperty("name")]
+        internal string Name { get; set; }
+
+        [JsonProperty("version_number")]
+        internal string VersionNumber { get; set; }
+
+        [JsonProperty("website_url")]
+        internal Uri WebsiteUrl { get; set; }
+
+        [JsonProperty("description")]
+        internal string Description { get; set; }
+
+        [JsonProperty("dependencies")]
+        internal List<string> Dependencies { get; set; }
+    }
+}
