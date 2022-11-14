1162baf58267b5276aa9d58b3b3f7cb15dbc579f
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-11-14 10:45:18 +0100

    Refactor into methods

--- BepInEx/Configuration/TomlTypeConverter.cs
+++ BepInEx/Configuration/TomlTypeConverter.cs
@@ -344,47 +344,43 @@ namespace BepInEx.Configuration
 			
 			TomlTypeConverter.AddConverter(typeof(Rect), new TypeConverter
 			{
-				ConvertToObject = delegate (string s, Type type)
-				{
-					Rect result = default(Rect);
-					if (s != null)
-					{
-						string cleaned = s.Trim(new char[] { '{', '}' }).Replace(" ", "");
-						foreach (string part in cleaned.Split(new char[] { ',' }))
-						{
-							string[] parts = part.Split(new char[] { ':' });
-							float value;
-							if (parts.Length == 2 && float.TryParse(parts[1], out value))
-							{
-								string id = parts[0].Trim(new char[] { '"' });
-								if (id == "x")
-								{
-									result.x = value;
-								}
-								else if (id == "y")
-								{
-									result.y = value;
-								}
-								else if (id == "width" || id == "z")
-								{
-									result.width = value;
-								}
-								else if (id == "height" || id == "w")
-								{
-									result.height = value;
-								}
-							}
-						}
-					}
-
-					return result;
-				},
-				ConvertToString = delegate (object o, Type type)
-				{
-					Rect rect = (Rect)o;
-					return string.Format(CultureInfo.InvariantCulture, "{{ \"x\":{0}, \"y\":{1}, \"width\":{2}, \"height\":{3} }}", new object[] { rect.x, rect.y, rect.width, rect.height });
-				}
+				ConvertToObject = StringToRect,
+				ConvertToString = RectToString
 			});
+		}
+
+		private static object StringToRect(string s, Type type)
+		{
+			Rect result = default(Rect);
+			
+			if (s == null)
+				return result;
+
+			string cleaned = s.Trim('{', '}').Replace(" ", "");
+			foreach (string part in cleaned.Split(','))
+			{
+				string[] parts = part.Split(':');
+				if (parts.Length != 2 || !float.TryParse(parts[1], out float value))
+					continue;
+
+				string id = parts[0].Trim('"');
+				if (id == "x")
+					result.x = value;
+				else if (id == "y")
+					result.y = value;
+				else if (id == "width" || id == "z")
+					result.width = value;
+				else if (id == "height" || id == "w")
+					result.height = value;
+			}
+
+			return result;
+		}
+
+		private static string RectToString(object o, Type type)
+		{
+			Rect rect = (Rect)o;
+			return string.Format(CultureInfo.InvariantCulture, "{{ \"x\":{0}, \"y\":{1}, \"width\":{2}, \"height\":{3} }}", new object[] { rect.x, rect.y, rect.width, rect.height });
 		}
 	}
 }
