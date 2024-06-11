851521cc126e4f9d841d2d9bfe857558f0395939
aldelaro5 <aldelaro5@gmail.com>
2024-06-11 12:00:31 -0400

    Upgrade Doorstop to 4.3.0 (#901)

--- build/Program.cs
+++ build/Program.cs
@@ -32,7 +32,7 @@ public class BuildContext : FrostingContext
         BleedingEdge
     }
 
-    public const string DoorstopVersion = "4.2.0";
+    public const string DoorstopVersion = "4.3.0";
     public const string DotnetRuntimeVersion = "6.0.7";
     public const string DobbyVersion = "1.0.5";
 
