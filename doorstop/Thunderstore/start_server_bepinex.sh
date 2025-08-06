3b4365d6ff7ff5c7aa331c895b32be0e6c989ed0
AzumattDev <azumattgaming@gmail.com>
2025-08-06 03:30:37 -0400

    Update start_server_bepinex.sh with new vars

--- doorstop/Thunderstore/start_server_bepinex.sh
+++ doorstop/Thunderstore/start_server_bepinex.sh
@@ -2,9 +2,8 @@
 # BepInEx-specific settings
 # NOTE: Do not edit unless you know what you are doing!
 ####
-export DOORSTOP_ENABLE=TRUE
-export DOORSTOP_INVOKE_DLL_PATH=./BepInEx/core/BepInEx.Preloader.dll
-export DOORSTOP_CORLIB_OVERRIDE_PATH=./unstripped_corlib
+export DOORSTOP_ENABLED=1
+export DOORSTOP_TARGET_ASSEMBLY=./BepInEx/core/BepInEx.Preloader.dll
 
 export LD_LIBRARY_PATH="./doorstop_libs:$LD_LIBRARY_PATH"
 export LD_PRELOAD="libdoorstop_x64.so:$LD_PRELOAD"
